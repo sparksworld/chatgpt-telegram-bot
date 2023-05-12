35032ee8240bdba20068710b588713170cf8a26d
spark <228436652@qq.com>
2023-05-12 19:33:11 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -74,7 +74,7 @@ def main():
         'token_price': float(os.environ.get('TOKEN_PRICE', 0.002)),
         'image_prices': [float(i) for i in os.environ.get('IMAGE_PRICES', "0.016,0.018,0.02").split(",")],
         'transcription_price': float(os.environ.get('TOKEN_PRICE', 0.006)),
-        'bot_language': os.environ.get('BOT_LANGUAGE', 'en'),
+        'bot_language': os.environ.get('BOT_LANGUAGE', 'zh-cn'),
     }
 
     # Setup and run ChatGPT and Telegram bot
